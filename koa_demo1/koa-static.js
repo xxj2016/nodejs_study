27f96d8dc5168e74bd846f154c904372c46cb52b
Jet Xu <2789292867@qq.com>
2020-03-03 17:56:51 +0800

    koa-static

---
+++ koa_demo1/koa-static.js
@@ -0,0 +1,50 @@
+/*
+    Koa 使用koa-bodyparser获取表单提交的数据
+
+    1. npm install --save koa-static
+
+    2. 引入 static = require('koa-static');
+
+    3. app.use(static('./static'));
+      
+    or app.usr(static(__dirname + '/static'))
+    
+*/
+
+
+var Koa = require("koa"),
+    router = require("koa-router")(),
+    views = require("koa-views"),
+    bodyParser = require('koa-bodyparser'),
+    static = require('koa-static');
+
+var app = new Koa();
+
+// 配置post bodyParser的中间件
+app.use(bodyParser());
+
+app.use(static('./static'));
+
+app.use(views('views', {
+    extension: 'ejs'
+}))
+
+
+router.get('/', async (ctx) => {
+    await ctx.render('form');
+})
+
+// 接受post提交的数据
+router.post('/doAdd', async (ctx) => {
+    // 原生nodejs在koa中获取表单提交的数据
+    // var data = await common.getPostData(ctx);
+
+    // koa-bodyParser获取表单提交的数据
+    var data = ctx.request.body;
+    console.log(data);
+    ctx.body = data;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
