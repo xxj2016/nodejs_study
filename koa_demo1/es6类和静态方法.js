422765aae1051a615a6611484243638417b9ae0c
Jet Xu <2789292867@qq.com>
2020-03-05 11:57:21 +0800

    es6继承

--- koa_demo1/es6类和静态方法.js
+++ koa_demo1/es6类和静态方法.js
@@ -1,7 +1,8 @@
 /* 定义Person类 */
 
-class Person {
-    constructor(name, age) { // 类的构造函数，实例化的时候执行，new的时候执行
+/* class Person {
+    constructor(name, age) {
+        // 类的构造函数，实例化的时候执行，new的时候执行
         this._name = name;
         this._age = age;
     }
@@ -14,10 +15,42 @@ class Person {
     setName(name) {
         this._name = name;
     }
-
 }
 
-var p = new Person('sdasd', 20);
-p.setName('Jet');
-p.getName();
+var p = new Person("sdasd", 20);
+p.setName("Jet");
+p.getName(); */
 
+
+
+// es6继承
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getInfo() {
+        console.log(`姓名：${this.name} --- 年龄：${this.age}`);
+    }
+
+    run() {
+        console.log("run");
+    }
+}
+
+class Web extends Person {
+    constructor(name, age, sex) {
+        super(name, age);
+        this.sex = sex;
+    }
+
+    print() {
+        console.log(`${this.name} --- ${this.age} --- ${this.sex}`);
+    }
+}
+
+var w = new Web("Jet", 30, "男");
+w.print();
+w.getInfo();
+w.run();
