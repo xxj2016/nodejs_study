e71ab10505a631f7fff5764f9c2bf128e7ad2142
Jet Xu <2789292867@qq.com>
2020-03-08 23:50:25 +0800

    nodejs connect mongodb

--- koa_demo1/nodejs_conect_mongodb.js
+++ koa_demo1/nodejs_conect_mongodb.js
@@ -25,3 +25,30 @@
         })
     })
 */
+
+const MongoClient = require('mongodb').MongoClient;
+
+const dbUrl = "mongodb://localhost:27017";
+
+const dbName = 'koa';
+
+// 连接数据库
+console.time('start');
+MongoClient.connect(dbUrl, (err, client) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var db = client.db(dbName);
+
+    // 增加数据
+    db.collection('user').insertOne({'username':'Xu','age': 32,'sex': '男','status': "1"}, function(err, result) {
+        if (!err) {
+            console.log('增加数据成功');
+
+            client.close();
+
+            console.timeEnd('start');
+        }
+    })
+})
