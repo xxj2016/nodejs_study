a7ee07d71c2387651ee8bae992c44d2f97592495
Jet Xu <2789292867@qq.com>
2020-03-21 23:46:10 +0800

    修改

--- koa2_demo1/routes/users.js
+++ koa2_demo1/routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function (ctx, next) {
 })
 
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+  ctx.body = 'test this is a users/bar response'
 })
 
 module.exports = router
